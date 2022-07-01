6c4810d49e4468e6b0443be6f0be6501f6a3e153
Sxmurai <gavrw0607@gmail.com>
2022-07-01 16:11:30 -0500

    version: 1.1.0

--- src/main/java/wtf/nebula/Nebula.java
+++ src/main/java/wtf/nebula/Nebula.java
@@ -26,7 +26,7 @@ import java.util.logging.Logger;
  */
 public class Nebula {
     public static final String NAME = "Nebula";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     public static LogAgent log;
     public static EventBus BUS;
