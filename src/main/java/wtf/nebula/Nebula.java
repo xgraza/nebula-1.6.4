16ebe03d99f27796c5ec3bdad372c24a642bd6fa
Sxmurai <gavrw0607@gmail.com>
2022-07-06 00:03:55 -0500

    impl: update client ver

--- src/main/java/wtf/nebula/Nebula.java
+++ src/main/java/wtf/nebula/Nebula.java
@@ -27,7 +27,7 @@ import java.util.logging.Logger;
  */
 public class Nebula {
     public static final String NAME = "Nebula";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.3.0";
     public static final String TAG = "rel"; // rel, beta, dev, rc(INT)
 
     public static LogAgent log;
