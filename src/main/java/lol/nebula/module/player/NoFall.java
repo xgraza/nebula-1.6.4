70f6037141c84998b4f663958ea8faf80f372823
Sxmurai <gavrw0607@gmail.com>
2023-04-30 14:48:06 -0500

    'Lag Back' mode in nofall

--- src/main/java/lol/nebula/module/player/NoFall.java
+++ src/main/java/lol/nebula/module/player/NoFall.java
@@ -6,11 +6,14 @@ import lol.nebula.module.Module;
 import lol.nebula.module.ModuleCategory;
 import lol.nebula.setting.Setting;
 
+import static net.minecraft.client.resources.I18n.format;
+
 /**
  * @author aesthetical
  * @since 04/29/23
  */
 public class NoFall extends Module {
+    private final Setting<Mode> mode = new Setting<>(Mode.SPOOF, "Mode");
     private final Setting<Float> distance = new Setting<>(3.0f, 0.01f, 3.0f, 40.0f, "Distance");
 
     public NoFall() {
@@ -19,14 +22,24 @@ public class NoFall extends Module {
 
     @Override
     public String getMetadata() {
-        return distance.getValue().toString();
+        return format("%s %.1f", Setting.formatEnumName(mode.getValue()), distance.getValue());
     }
 
     @Listener
     public void onWalkingUpdate(EventWalkingUpdate event) {
         if (mc.thePlayer.fallDistance >= distance.getValue()) {
-            event.setOnGround(true);
+
             mc.thePlayer.fallDistance = 0.0f;
+            if (mode.getValue() == Mode.SPOOF) {
+                event.setOnGround(true);
+            } else if (mode.getValue() == Mode.LAG_BACK) {
+                event.setY(event.getY() + 10.0);
+                event.setStance(event.getStance() + 10.0);
+            }
         }
+    }
+
+    public enum Mode {
+        SPOOF, LAG_BACK
     }
 }
