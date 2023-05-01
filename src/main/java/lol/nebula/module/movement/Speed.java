51f5c88cf54ace53c23dd179505b132593ced073
Sxmurai <gavrw0607@gmail.com>
2023-05-01 16:30:22 -0500

    damage boost

--- src/main/java/lol/nebula/module/movement/Speed.java
+++ src/main/java/lol/nebula/module/movement/Speed.java
@@ -4,10 +4,14 @@ import lol.nebula.listener.bus.Listener;
 import lol.nebula.listener.events.entity.EventMove;
 import lol.nebula.listener.events.entity.EventUpdate;
 import lol.nebula.listener.events.entity.EventWalkingUpdate;
+import lol.nebula.listener.events.net.EventPacket;
 import lol.nebula.module.Module;
 import lol.nebula.module.ModuleCategory;
 import lol.nebula.setting.Setting;
+import lol.nebula.util.math.timing.Timer;
 import lol.nebula.util.player.MoveUtils;
+import net.minecraft.network.play.server.S12PacketEntityVelocity;
+import net.minecraft.network.play.server.S27PacketExplosion;
 
 /**
  * @author aesthetical
@@ -15,10 +19,14 @@ import lol.nebula.util.player.MoveUtils;
  */
 public class Speed extends Module {
     private final Setting<Mode> mode = new Setting<>(Mode.HOP, "Mode");
+    private final Setting<Boolean> damageBoost = new Setting<>(true, "Damage Boost");
 
     private double distance, speed;
     private int stage;
     private boolean boost;
+
+    private final Timer damageBoostTimer = new Timer();
+    private int damageBoostTicks;
 
     public Speed() {
         super("Speed", "oooah she a trackstar", ModuleCategory.MOVEMENT);
@@ -32,13 +40,14 @@ public class Speed extends Module {
         distance = 0.0;
         speed = 0.0;
         boost = false;
+        damageBoostTicks = 0;
 
         mc.timer.timerSpeed = 1.0f;
     }
 
     @Override
     public String getMetadata() {
-        return Setting.formatEnumName(mode.getValue());
+        return (damageBoostTicks > 0 ? "Boosted " : "") + Setting.formatEnumName(mode.getValue());
     }
 
     @Listener
@@ -97,10 +106,17 @@ public class Speed extends Module {
                 boost = !boost;
                 stage = 4;
             } else {
-                speed -= speed / 159.0;
+                if (damageBoostTicks > 0) {
+                    speed -= speed / 100.0;
+                } else {
+                    speed -= speed / 159.0;
+                }
             }
 
             speed = Math.max(speed, MoveUtils.getBaseNcpSpeed(20));
+            if (damageBoostTicks-- > 0) {
+                speed = 1.99 * MoveUtils.getBaseNcpSpeed(20);
+            }
 
             if (MoveUtils.isMoving()) {
                 MoveUtils.strafe(event, speed);
@@ -119,6 +135,27 @@ public class Speed extends Module {
         double x = mc.thePlayer.posX - mc.thePlayer.prevPosX;
         double z = mc.thePlayer.posZ - mc.thePlayer.prevPosZ;
         distance = Math.sqrt(x * x + z * z);
+    }
+
+    @Listener(receiveCanceled = true)
+    public void onPacketInbound(EventPacket.Inbound event) {
+        if (!damageBoostTimer.ticks(40, false)) return;
+
+        if (event.getPacket() instanceof S12PacketEntityVelocity) {
+            S12PacketEntityVelocity packet = event.getPacket();
+            if (damageBoost.getValue() && packet.func_149412_c() == mc.thePlayer.getEntityId()) {
+                damageBoostTimer.resetTime();
+                damageBoostTicks = 10;
+            }
+        } else if (event.getPacket() instanceof S27PacketExplosion) {
+            S27PacketExplosion packet = event.getPacket();
+
+            double dist = mc.thePlayer.getDistanceSq(packet.func_149148_f(), packet.func_149143_g(), packet.func_149145_h());
+            if (damageBoost.getValue() && dist < 6 * 6) {
+                damageBoostTimer.resetTime();
+                damageBoostTicks = 10;
+            }
+        }
     }
 
     public enum Mode {
