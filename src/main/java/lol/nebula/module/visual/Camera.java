5ffc6524de8a6e057c56da8dc361f5f64d3cadc8
Sxmurai <gavrw0607@gmail.com>
2023-05-07 12:17:04 -0500

    start of camera module

---
+++ src/main/java/lol/nebula/module/visual/Camera.java
@@ -0,0 +1,14 @@
+package lol.nebula.module.visual;
+
+import lol.nebula.module.Module;
+import lol.nebula.module.ModuleCategory;
+
+/**
+ * @author aesthetical
+ * @since 05/07/23
+ */
+public class Camera extends Module {
+    public Camera() {
+        super("Camera", "Camera tweaks", ModuleCategory.VISUAL);
+    }
+}
