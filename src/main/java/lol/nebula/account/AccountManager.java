5c749264805a05b0794a5fadd7384d23cbd31ac4
Sxmurai <gavrw0607@gmail.com>
2023-05-07 14:19:01 -0500

    account config and more api usage for Account Manager

--- src/main/java/lol/nebula/account/AccountManager.java
+++ src/main/java/lol/nebula/account/AccountManager.java
@@ -1,5 +1,8 @@
 package lol.nebula.account;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.Session;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,12 +13,26 @@ import java.util.List;
 public class AccountManager {
 
     /**
+     * Gets the minecraft game instance
+     */
+    private final Minecraft mc = Minecraft.getMinecraft();
+
+    /**
      * The account cache
      */
     private final List<Account> accountList = new ArrayList<>();
 
     public AccountManager() {
         new AccountsConfig(this);
+    }
+
+    /**
+     * Logs into an account
+     * @param account the account object
+     */
+    public void login(Account account) {
+        // im not even gonna bother with MSA auth
+        mc.setSession(new Session(account.getEmail(), "", ""));
     }
 
     /**
