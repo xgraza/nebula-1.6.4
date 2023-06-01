36dd60ef7df7af032efab3b2b799ab8dfc733435
Sxmurai <gavrw0607@gmail.com>
2023-05-31 23:20:26 -0500

    start of auto fish

---
+++ src/main/java/lol/nebula/module/player/AutoFish.java
@@ -0,0 +1,15 @@
+package lol.nebula.module.player;
+
+import lol.nebula.module.Module;
+import lol.nebula.module.ModuleCategory;
+
+/**
+ * @author aesthetical
+ * @since 05/31/23
+ */
+public class AutoFish extends Module {
+
+    public AutoFish() {
+        super("Auto Fish", "Automatically fishes for you", ModuleCategory.PLAYER);
+    }
+}
