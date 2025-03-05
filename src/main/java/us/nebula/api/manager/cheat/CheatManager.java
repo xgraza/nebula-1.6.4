f09c412857a2fd3284424204e7d237370721fe87
aestheticalll <gavrw0607@gmail.com>
2025-03-05 17:25:32 -0600

    impl: KeyPearl cheat

--- src/main/java/us/nebula/api/manager/cheat/CheatManager.java
+++ src/main/java/us/nebula/api/manager/cheat/CheatManager.java
@@ -9,6 +9,7 @@ import us.nebula.impl.cheat.exploit.*;
 import us.nebula.impl.cheat.movement.*;
 import us.nebula.impl.cheat.player.AutoRespawnCheat;
 import us.nebula.impl.cheat.player.InfiniteMoverCheat;
+import us.nebula.impl.cheat.player.KeyPearlCheat;
 import us.nebula.impl.cheat.player.ScaffoldCheat;
 import us.nebula.impl.cheat.render.*;
 
@@ -45,6 +46,7 @@ public final class CheatManager implements ITypedManager<Cheat>
         addCheat(new SprintCheat());
         addCheat(new AutoRespawnCheat());
         addCheat(new InfiniteMoverCheat());
+        addCheat(new KeyPearlCheat());
         addCheat(new ScaffoldCheat());
         addCheat(new ClickGUICheat());
         addCheat(new FullbrightCheat());
---
+++ src/main/java/us/nebula/impl/cheat/player/KeyPearlCheat.java
@@ -0,0 +1,56 @@
+package us.nebula.impl.cheat.player;
+
+import net.minecraft.item.ItemEnderPearl;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
+import net.minecraft.network.play.client.C09PacketHeldItemChange;
+import us.nebula.api.manager.cheat.Cheat;
+import us.nebula.api.manager.cheat.CheatCategory;
+import us.nebula.api.manager.cheat.CheatManifest;
+
+/**
+ * @author xgraza
+ * @since 03/05/25
+ */
+@CheatManifest(name = "KeyPearl",
+        description = "Throws an ender pearl on a key press",
+        category = CheatCategory.PLAYER)
+public final class KeyPearlCheat extends Cheat
+{
+    @Override
+    protected void onEnable()
+    {
+        super.onEnable();
+
+        toggle();
+        if (MC.thePlayer == null || MC.theWorld == null)
+        {
+            return;
+        }
+
+        final int pearlSlot = getPearlSlot();
+        if (pearlSlot == -1)
+        {
+            return;
+        }
+
+        MC.thePlayer.sendQueue.addToSendQueue(new C09PacketHeldItemChange(pearlSlot));
+        MC.thePlayer.sendQueue.addToSendQueue(new C08PacketPlayerBlockPlacement(
+                MC.thePlayer.inventory.getStackInSlot(pearlSlot)));
+        MC.thePlayer.sendQueue.addToSendQueue(new C09PacketHeldItemChange(
+                MC.thePlayer.inventory.currentItem));
+    }
+
+    private int getPearlSlot()
+    {
+        for (int i = 0; i < 9; ++i)
+        {
+            final ItemStack itemStack = MC.thePlayer.inventory.getStackInSlot(i);
+            if (itemStack != null && itemStack.getItem() instanceof ItemEnderPearl)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
