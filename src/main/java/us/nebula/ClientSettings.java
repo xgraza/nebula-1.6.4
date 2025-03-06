036b9d6c55fb1188bde02d93123fb1cd475e6791
aestheticalll <gavrw0607@gmail.com>
2025-03-06 11:45:12 -0600

    impl: ClientSettings (best coding practices)

---
+++ src/main/java/us/nebula/ClientSettings.java
@@ -0,0 +1,14 @@
+package us.nebula;
+
+/**
+ * @author xgraza
+ * @since 03/05/25
+ * best coding practices here!
+ */
+public final class ClientSettings
+{
+    /**
+     * If features should use verbose logging
+     */
+    public static boolean VERBOSE_LOGGING;
+}
