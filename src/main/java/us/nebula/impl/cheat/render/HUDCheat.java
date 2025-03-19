29df6609c7cc9855d005052983b67b021b72d656
aestheticalll <gavrw0607@gmail.com>
2025-03-19 15:11:42 -0500

    fix: early return in rendering GUI overlays

--- src/main/java/us/nebula/impl/cheat/render/HUDCheat.java
+++ src/main/java/us/nebula/impl/cheat/render/HUDCheat.java
@@ -42,7 +42,7 @@ public final class HUDCheat extends Cheat
         {
             if (!overlay.getStateSetting().getValue())
             {
-                return;
+                continue;
             }
             overlay.render(event.getResolution(), event.getPartialTicks());
         }
