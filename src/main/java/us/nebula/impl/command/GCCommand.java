86a0dcdb80ca47116ec0fae49c5a820101ede337
aestheticalll <gavrw0607@gmail.com>
2025-03-24 14:02:47 -0500

    impl: GCCommand

---
+++ src/main/java/us/nebula/impl/command/GCCommand.java
@@ -0,0 +1,24 @@
+package us.nebula.impl.command;
+
+import us.nebula.api.manager.command.Command;
+import us.nebula.api.manager.command.CommandManifest;
+import us.nebula.api.manager.command.CommandResult;
+
+/**
+ * @author xgraza
+ * @since 03/24/25
+ */
+@CommandManifest(aliases = {"gc"},
+        description = "Runs the garbage collector")
+public final class GCCommand extends Command
+{
+    @Override
+    public void build()
+    {
+        argumentBuilder.dispatchSingle(() ->
+        {
+            System.gc();
+            return CommandResult.SUCCESS_DEFAULT;
+        });
+    }
+}
