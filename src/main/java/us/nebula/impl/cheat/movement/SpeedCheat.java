f079320251a747c529a5c036a5fb396f18526e8d
aestheticalll <gavrw0607@gmail.com>
2025-03-26 12:43:28 -0500

    impl: rewrite speed & make more stable

--- src/main/java/us/nebula/impl/cheat/movement/SpeedCheat.java
+++ src/main/java/us/nebula/impl/cheat/movement/SpeedCheat.java
@@ -1,22 +1,20 @@
 package us.nebula.impl.cheat.movement;
 
 import net.minecraft.network.play.server.S08PacketPlayerPosLook;
-import us.nebula.ClientSettings;
 import us.nebula.api.listener.EventListener;
 import us.nebula.api.listener.Subscribe;
 import us.nebula.api.manager.cheat.Cheat;
 import us.nebula.api.manager.cheat.CheatCategory;
 import us.nebula.api.manager.cheat.CheatManifest;
 import us.nebula.api.value.Setting;
-import us.nebula.impl.event.game.EventUpdate;
 import us.nebula.impl.event.network.EventPacket;
 import us.nebula.impl.event.player.EventMove;
-import us.nebula.util.player.ChatUtil;
+import us.nebula.impl.event.player.EventMoveUpdate;
 import us.nebula.util.player.MoveUtil;
 
 /**
  * @author xgraza
- * @since 03/17/25
+ * @since 03/26/25
  */
 @CheatManifest(name = "Speed",
         description = "Allows you to move faster",
@@ -25,110 +23,94 @@ public final class SpeedCheat extends Cheat
 {
     private final Setting<Mode> modeSetting = new Setting<>(
             "Mode", Mode.STRAFE);
-    private final Setting<Boolean> handleLagbacksSetting = new Setting<>(
-            "Handle Lagbacks", true);
 
-    private double speed, distanceTraveled;
-    private int strafeStage, lagbackTimer;
+    private double lastDistance, speed;
+    private int lagTicks, stage;
     private boolean boost;
 
     @Override
     protected void onDisable()
     {
         super.onDisable();
+        lastDistance = 0.0;
         speed = 0.0;
-        distanceTraveled = 0.0;
-        strafeStage = 0;
-        lagbackTimer = 0;
+        lagTicks = 0;
         boost = false;
         MC.timer.timerSpeed = 1.0f;
     }
 
     @Subscribe
-    private final EventListener<EventUpdate> updateEventListener = event ->
+    private final EventListener<EventMove> moveEventListener = event ->
     {
-        distanceTraveled = MoveUtil.getPlayerMoveDistance();
-
-        if (modeSetting.getValue() == Mode.Y_PORT && MoveUtil.isMoving())
+        if (--lagTicks > 0)
         {
-            if (--lagbackTimer > 0)
+            return;
+        }
+
+        if (modeSetting.getValue() == Mode.STRAFE)
+        {
+            if (MoveUtil.isMoving() && MC.thePlayer.onGround)
             {
-                return;
+                stage = 0;
             }
-            MC.thePlayer.setSprinting(true);
-            speed = 1.2 * MoveUtil.getBaseNcpSpeed(0) - 0.04;
-            MC.thePlayer.setSprinting(true);
-            if (MC.thePlayer.onGround)
+
+            switch (stage)
             {
-                MC.thePlayer.motionY = 0.42f;
-                speed *= boost ? 1.62 : 1.526;
-                strafeStage = 0;
-            } else
+                case 0:
+                case 1:
+                {
+                    if (stage == 0)
+                    {
+                        MC.timer.timerSpeed = 1.0f;
+                        speed = 1.2945 * MoveUtil.getBaseNcpSpeed(20) - 0.01;
+                        stage = 1;
+                    }
+                    if (MoveUtil.isMoving() && MC.thePlayer.onGround)
+                    {
+                        MC.thePlayer.motionY = MoveUtil.getJumpHeight(0.4f);
+                        event.setY(MC.thePlayer.motionY);
+                        speed *= boost ? 1.655 : 1.48;
+                        MC.timer.timerSpeed = boost ? 1.044f : 1.088f;
+                        stage = 2;
+                    }
+                    break;
+                }
+                case 2:
+                {
+                    final double mod = boost ? 0.76 : 0.66;
+                    final double diff = mod * (speed - MoveUtil.getBaseNcpSpeed(20));
+                    speed = lastDistance - diff;
+                    stage = 3;
+                    boost = !boost;
+                    break;
+                }
+                case 3:
+                {
+                    MC.timer.timerSpeed = 1.0f;
+                    if (!MoveUtil.isMoving() && MC.thePlayer.onGround)
+                    {
+                        stage = 0;
+                    }
+                    speed = speed - speed / 159.0;
+                    break;
+                }
+            }
+
+            speed = Math.max(speed, MoveUtil.getBaseNcpSpeed(20));
+
+            if (MoveUtil.isMoving())
             {
-                MC.thePlayer.motionY = -1;
-                boost = !boost;
+                MoveUtil.setSpeed(event, speed);
             }
         }
     };
 
     @Subscribe
-    private final EventListener<EventMove> moveEventListener = event ->
+    private final EventListener<EventMoveUpdate> moveUpdateEventListener = event ->
     {
-        if (--lagbackTimer > 0)
-        {
-            return;
-        }
-
-        switch (modeSetting.getValue())
-        {
-            case STRAFE:
-            {
-                if (speed <= 0.0)
-                {
-                    speed = 1.38 * MoveUtil.getBaseNcpSpeed(10) - 0.1;
-                }
-
-                if (MC.thePlayer.onGround && MoveUtil.isMoving())
-                {
-                    strafeStage = 0;
-                }
-
-                switch (strafeStage++)
-                {
-                    case 0:
-                    {
-                        if (MC.thePlayer.onGround && MoveUtil.isMoving())
-                        {
-                            MC.thePlayer.motionY = MoveUtil.getJumpHeight(0.3995);
-                            event.setY(MC.thePlayer.motionY);
-                            speed *= boost ? 1.624 : 1.543;
-                        }
-                        break;
-                    }
-                    case 1:
-                    {
-                        final double modifier = boost ? 0.72 : 0.66;
-                        speed = distanceTraveled - (modifier * (distanceTraveled - MoveUtil.getBaseNcpSpeed(10)));
-                        boost = !boost;
-                        break;
-                    }
-                    default:
-                    {
-                        speed -= speed / 159.0;
-                    }
-                }
-                break;
-            }
-            case Y_PORT:
-            {
-                break;
-            }
-        }
-
-        if (MoveUtil.isMoving())
-        {
-            MoveUtil.setSpeed(event, speed);
-        }
+        final double diffX = MC.thePlayer.posX - MC.thePlayer.lastTickPosX;
+        final double diffZ = MC.thePlayer.posZ - MC.thePlayer.lastTickPosZ;
+        lastDistance = Math.sqrt(diffX * diffX + diffZ * diffZ);
     };
 
     @Subscribe
@@ -136,20 +118,12 @@ public final class SpeedCheat extends Cheat
     {
         if (event.getPacket() instanceof S08PacketPlayerPosLook)
         {
-            if (!handleLagbacksSetting.getValue())
-            {
-                return;
-            }
-            if (ClientSettings.VERBOSE_LOGGING)
-            {
-                ChatUtil.send("Setting lagback ticks to 10...");
-            }
-            lagbackTimer = 10;
+            lagTicks = 8;
         }
     };
 
     private enum Mode
     {
-        STRAFE, Y_PORT
+        STRAFE
     }
 }
