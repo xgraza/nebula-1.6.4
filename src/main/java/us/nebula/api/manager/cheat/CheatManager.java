f2488db86a871e5e5c87bb40d9a8fc603768d46e
aestheticalll <gavrw0607@gmail.com>
2025-04-03 09:29:07 -0500

    impl: NoC03

--- src/main/java/us/nebula/api/manager/cheat/CheatManager.java
+++ src/main/java/us/nebula/api/manager/cheat/CheatManager.java
@@ -41,6 +41,7 @@ public final class CheatManager implements ITypedManager<Cheat>
         addCheat(new FrankyCheat());
         addCheat(new GhostHandCheat());
         addCheat(new NewChunksCheat());
+        addCheat(new NoC03Cheat());
         addCheat(new NoHungerCheat());
         addCheat(new NoPortalGUICheat());
         addCheat(new PotionSaverCheat());
