0ba3268ab59e7967591ba7c32b595047b73206b5
aestheticalll <gavrw0607@gmail.com>
2025-04-07 07:41:52 -0500

    fix: wrong enchantment calc in AutoArmor

--- src/main/java/us/nebula/impl/cheat/combat/AutoArmorCheat.java
+++ src/main/java/us/nebula/impl/cheat/combat/AutoArmorCheat.java
@@ -140,7 +140,7 @@ public final class AutoArmorCheat extends Cheat
         float score = armor.damageReduceAmount;
         for (final Enchantment enchantment : RELEVANT_ENCHANTMENTS)
         {
-            float enchantmentLevel = ItemUtil.getEnchantLevel(enchantment, itemStack);
+            float enchantmentLevel = ItemUtil.getEnchantLevelNoLimit(enchantment, itemStack);
             if (enchantmentLevel == 0.0f)
             {
                 continue;
