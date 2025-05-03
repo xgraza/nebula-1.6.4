e70d2aa036f41a2b4095ac5d34e51d5b39e1e13d
aestheticalll <gavrw0607@gmail.com>
2025-05-02 19:17:44 -0500

    impl: AutoRespawn write coordinates on death to file

--- src/main/java/us/nebula/impl/cheat/player/AutoRespawnCheat.java
+++ src/main/java/us/nebula/impl/cheat/player/AutoRespawnCheat.java
@@ -1,6 +1,7 @@
 package us.nebula.impl.cheat.player;
 
 import net.minecraft.network.play.client.C16PacketClientStatus;
+import us.nebula.Nebula;
 import us.nebula.api.listener.EventListener;
 import us.nebula.api.listener.Subscribe;
 import us.nebula.api.manager.cheat.Cheat;
@@ -9,6 +10,13 @@ import us.nebula.api.manager.cheat.CheatManifest;
 import us.nebula.api.value.Setting;
 import us.nebula.impl.event.player.EventPlayerDeath;
 import us.nebula.util.player.ChatUtil;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author xgraza
@@ -19,6 +27,66 @@ import us.nebula.util.player.ChatUtil;
         category = CheatCategory.PLAYER)
 public final class AutoRespawnCheat extends Cheat
 {
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+    private static final DateFormat TIME_FORMAT = new SimpleDateFormat("kk:mm:ss");
+    private static final File COORDINATE_SAVE_FILE;
+    private static OutputStream OPEN_FILE_STREAM;
+
+    static
+    {
+        final File coordinateLogFolder = new File(Nebula.INSTANCE.getNebulaRootDir(), "respawn_coords");
+        if (!coordinateLogFolder.exists())
+        {
+            if (!coordinateLogFolder.mkdir())
+            {
+                throw new RuntimeException("Failed to create " + coordinateLogFolder);
+            }
+        }
+
+        int index = 0;
+        File file = null;
+        while (file == null)
+        {
+            String name = "coordinates_" + DATE_FORMAT.format(new Date());
+            if (index > 0)
+            {
+                name += "_session_" + index;
+            }
+            final File logFile = new File(coordinateLogFolder, name);
+            if (logFile.exists())
+            {
+                ++index;
+                continue;
+            }
+            file = logFile;
+        }
+
+        COORDINATE_SAVE_FILE = file;
+        try
+        {
+            COORDINATE_SAVE_FILE.createNewFile();
+            OPEN_FILE_STREAM = Files.newOutputStream(COORDINATE_SAVE_FILE.toPath());
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() ->
+            {
+                if (OPEN_FILE_STREAM != null)
+                {
+                    try
+                    {
+                        OPEN_FILE_STREAM.flush();
+                        OPEN_FILE_STREAM.close();
+                    } catch (IOException e)
+                    {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }, "AutoRespawn-File-Writer"));
+        } catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
     private final Setting<Boolean> logCoordsSetting = new Setting<>("Log Coordinates", false);
 
     @Subscribe
@@ -28,11 +96,29 @@ public final class AutoRespawnCheat extends Cheat
         {
             if (logCoordsSetting.getValue())
             {
-                // TODO: save to file...
-                ChatUtil.send("Saving coordinates at XYZ: %.1f, %.1f, %.1f", MC.thePlayer.posX, MC.thePlayer.boundingBox.minY, MC.thePlayer.posZ);
+                writeCoordsToFile(String.format("%.2f, %.2f, %.2f", MC.thePlayer.posX, MC.thePlayer.boundingBox.minY, MC.thePlayer.posZ));
+                ChatUtil.send("Writing coordinates to file...");
             }
             MC.thePlayer.sendQueue.addToSendQueue(new C16PacketClientStatus(
                     C16PacketClientStatus.EnumState.PERFORM_RESPAWN));
         }
     };
+
+    private void writeCoordsToFile(final String coordinates)
+    {
+        if (OPEN_FILE_STREAM == null)
+        {
+            return;
+        }
+        final String writeStr = TIME_FORMAT.format(new Date()) + " -> " + coordinates + "\n";
+        final byte[] bytes = writeStr.getBytes(StandardCharsets.UTF_8);
+        try
+        {
+            OPEN_FILE_STREAM.write(bytes, 0, bytes.length);
+            OPEN_FILE_STREAM.flush();
+        } catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
 }
