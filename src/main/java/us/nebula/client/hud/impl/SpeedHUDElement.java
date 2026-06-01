85e798f37ecf58420ef32df5a62023a4c726e6fe
xgraza <gavrw0607@gmail.com>
2026-05-31 19:49:06 -0500

    fix(SpeedHUD): make it work :)

--- src/main/java/us/nebula/client/hud/impl/SpeedHUDElement.java
+++ src/main/java/us/nebula/client/hud/impl/SpeedHUDElement.java
@@ -5,10 +5,6 @@ import net.minecraft.util.EnumChatFormatting;
 import us.nebula.client.cheat.impl.render.HUDCheat;
 import us.nebula.client.hud.HUDElement;
 import us.nebula.client.hud.HUDManifest;
-import us.nebula.client.listener.EventListener;
-import us.nebula.client.listener.Subscribe;
-import us.nebula.client.listener.event.game.EventUpdate;
-import us.nebula.client.util.player.ChatUtil;
 import us.nebula.client.util.render.gui.font.Fonts;
 
 /**
@@ -20,19 +16,6 @@ import us.nebula.client.util.render.gui.font.Fonts;
         x = 2, y = 2)
 public final class SpeedHUDElement extends HUDElement
 {
-    private double speed;
-
-    @Subscribe
-    private final EventListener<EventUpdate> updateEventListener = event ->
-    {
-        final double deltaX = MC.thePlayer.posX - MC.thePlayer.lastTickPosX;
-        final double deltaZ = MC.thePlayer.posZ - MC.thePlayer.lastTickPosZ;
-        final double moveDelta = Math.sqrt(deltaX * deltaX + deltaZ * deltaZ);
-        speed = (moveDelta / 1000) / (0.05 / 3600);
-        speed *= MC.timer.timerSpeed;
-        speed /= 3.6;
-    };
-
     @Override
     public void init()
     {
@@ -43,6 +26,12 @@ public final class SpeedHUDElement extends HUDElement
     @Override
     public void render(final ScaledResolution res)
     {
+        final double deltaX = MC.thePlayer.posX - MC.thePlayer.lastTickPosX;
+        final double deltaZ = MC.thePlayer.posZ - MC.thePlayer.lastTickPosZ;
+        final double moveDelta = Math.sqrt(deltaX * deltaX + deltaZ * deltaZ);
+        double speed = (moveDelta / 1000) / (0.05 / 3600);
+        speed *= MC.timer.timerSpeed;
+        speed /= 3.6;
         final String formatted = String.format("Speed: %s%.2f BPS",
                 EnumChatFormatting.GRAY, speed);
         setWidth(Fonts.POPPINS.getStringWidth(formatted) + (getPadding() * 4));
