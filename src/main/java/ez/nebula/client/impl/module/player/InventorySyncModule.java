6f0779774ff6c32fb03b37aa66f49cc450b7c20b
xgraza <gavrw0607@gmail.com>
2026-06-23 18:55:03 -0500

    refactor(InventorySync): Remove "Packet" option (as it does nothing of value)

--- src/main/java/ez/nebula/client/impl/module/player/InventorySyncModule.java
+++ src/main/java/ez/nebula/client/impl/module/player/InventorySyncModule.java
@@ -2,14 +2,12 @@ package ez.nebula.client.impl.module.player;
 
 import ez.nebula.client.api.manager.module.Module;
 import net.minecraft.inventory.Container;
-import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
 import net.minecraft.network.play.server.S32PacketConfirmTransaction;
 import ez.nebula.client.api.listener.EventListener;
 import ez.nebula.client.api.listener.Subscribe;
 import ez.nebula.client.api.manager.module.trait.ModuleCategory;
 import ez.nebula.client.api.manager.module.trait.ModuleManifest;
 import ez.nebula.client.api.listener.event.network.EventPacket;
-import ez.nebula.client.api.setting.Setting;
 
 /**
  * @author xgraza
@@ -20,10 +18,6 @@ import ez.nebula.client.api.setting.Setting;
         category = ModuleCategory.PLAYER)
 public final class InventorySyncModule extends Module
 {
-    private final Setting<Boolean> packetSetting = builder("Packet", false)
-            .setDescription("If to send a packet to try to further sync your inventory")
-            .build();
-
     @Subscribe
     private final EventListener<EventPacket.Inbound> inboundEventListener = event ->
     {
@@ -40,10 +34,6 @@ public final class InventorySyncModule extends Module
             if (transaction + 1 <= currentTransaction || transaction > currentTransaction)
             {
                 container.transactionID = (short) (transaction + 1);
-            }
-            if (packetSetting.getValue())
-            {
-                MC.thePlayer.sendQueue.addToSendQueue(new C08PacketPlayerBlockPlacement());
             }
         }
     };
