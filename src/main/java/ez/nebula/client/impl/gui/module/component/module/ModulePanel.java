80342bdefee71a0c309bd662adee72c764dd3804
xgraza <gavrw0607@gmail.com>
2026-07-25 18:00:48 -0500

    fix(ModulePanel): Fix padding for keybind box render on module

--- src/main/java/ez/nebula/client/impl/gui/module/component/module/ModulePanel.java
+++ src/main/java/ez/nebula/client/impl/gui/module/component/module/ModulePanel.java
@@ -148,8 +148,8 @@ public final class ModulePanel extends GUIComponent implements IGUIInputListener
         final double boxPosX = (x + width) - boxWidth - offset;
         final double boxPosY = y - (middlePoint - ((boxHeight - (PADDING * 2)) / 2.0));
 
-        RenderUtil.renderRoundedRectangle(boxPosX, boxPosY, boxWidth, boxHeight, 3.5f, KEY_BACKGROUND_COLOR);
-        Fonts.POPPINS_SMALL.drawStringShadow(text, boxPosX + (PADDING * 2), boxPosY + PADDING, -1);
+        RenderUtil.renderRoundedRectangle(boxPosX, boxPosY, boxWidth, boxHeight, 4.5f, KEY_BACKGROUND_COLOR);
+        Fonts.POPPINS_SMALL.drawStringShadow(text, boxPosX + (PADDING * 1.5), boxPosY + PADDING, -1);
     }
 
     @Override
