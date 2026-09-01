493ba58c895a6139994edaedfe12143630a3d0c0
xgraza <gavrw0607@gmail.com>
2026-09-01 11:19:47 -0500

    fix(ItemUtil): Incorrectly stating over stacked items are infinites

--- src/main/java/ez/nebula/client/util/minecraft/player/ItemUtil.java
+++ src/main/java/ez/nebula/client/util/minecraft/player/ItemUtil.java
@@ -86,9 +86,7 @@ public final class ItemUtil
 
     public static boolean isInfinite(final ItemStack itemStack)
     {
-        return itemStack != null
-                && (itemStack.stackSize < 0
-                || itemStack.stackSize > itemStack.getMaxStackSize());
+        return itemStack != null && itemStack.stackSize < 0;
     }
 
     public static boolean is32k(final ItemStack itemStack)
