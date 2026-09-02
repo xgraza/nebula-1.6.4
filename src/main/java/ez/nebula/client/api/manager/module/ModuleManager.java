1977cc9e6208d7ddeae2bed2a67235374a78d8b9
xgraza <gavrw0607@gmail.com>
2026-09-02 11:58:53 -0500

    refactor: Remove WIP module

--- src/main/java/ez/nebula/client/api/manager/module/ModuleManager.java
+++ src/main/java/ez/nebula/client/api/manager/module/ModuleManager.java
@@ -59,7 +59,6 @@ public final class ModuleManager implements ITypedManager<Module>
         registerModule(new NoPacketKickModule());
         registerModule(new NoRotateSetModule());
         registerModule(new PacketCancellerModule());
-        registerModule(new PearlRejectModule());
         registerModule(new PhaseModule());
         registerModule(new PortalsModule());
         registerModule(new PotionSaverModule());
